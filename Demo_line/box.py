a0111ca55b010c884c904043cc8a8fe6d9b38727
Ievhen <grabowski.ievhen@gmail.com>
2020-07-09 11:20:42 +0200

    add -> initial function implementation, change -> box.run method

--- Demo_line/box.py
+++ Demo_line/box.py
@@ -3,6 +3,7 @@
 
 from Demo_line.functions import *
 
+
 class Box:
 
     def __init__(self, box_id, path):
@@ -10,31 +11,42 @@ class Box:
         self.path = path
 
     def run(self, section):
-        process1(section)  # pop from portal
-        check_and_pass(section)
-        process2()  # delta
-        process3()  # bufor s3
-        process4()  # APAS
-        process5()  # feader
-        process6()  # emc
+        process1(section, 0)  # get the product out of supermarket
+        check_and_pass(section, 0, 1)
+        process2(section)  # delta
+        check_and_pass(section, 1, 2)
+        process3(section)  # buffer s3
+        check_and_pass(section, 2, 3)
+        process4(section)  # apas
+        check_and_pass(section, 3, 4)
+        process5(section)  # feeder
+        check_and_pass(section, 4, 5)
+        process6(section)  # emc press
 
-        if self.path == 0:  # without NEXO
-            lift_enter_switch()
-            process7()  # cartesian
-            lift_exit_switch()
-        else:
-            lift_enter_switch()
-            process8()  # bufor s8
+        if self.path == 0:  # path to automatic workstation
+            lift_enter_switch(section, self.path)
+            process7(section)  # cartesian
+            lift_exit_switch(section, self.path)
+        else:  # path to manual workstation
+            lift_enter_switch(section, self.path)
+            process8(section)  # buffer s8
+            check_and_pass(section, 3, 4)
             wait_nexo_button()
-            process9()  # NEXO
+            check_and_pass(section, 7, 8)
+            process9(section)  # nexo
             wait_nexo_button()
-            process10()  # bufor s10
-            lift_exit_switch()
-        process11()  # bufor s11
-        process12()  # grawer
-        process13()  # BIG bufor s13
-        process14()  # buffor s14
-        process1(1)
+            check_and_pass(section, 8, 9)
+            process10(section)  # buffer s10
+            lift_exit_switch(section, self.path)
+
+        process11(section)  # buffer s11
+        check_and_pass(section, 10, 11)
+        process12(section)  # engraver
+        check_and_pass(section, 11, 12)
+        process13(section)  # big buffer s13
+        process14(section)  # buffer s14
+        check_and_pass(section, 13, 0)
+        process1(section, 1)  # put the product in the supermarket
 
     # setters
     def set_box_id(self, id):
--- Demo_line/functions.py
+++ Demo_line/functions.py
@@ -8,7 +8,6 @@ def check_status(section_id):
 
 # transfer the signal to Master or/and DB
 # WE are using TIMER and COIL to serve the signal
-
 def pass_unit(stop_gate):
     pass
 
@@ -24,7 +23,6 @@ def read_rfid():
 # transfer the signal to Master or/and DB
 # WE are using local state machine on Slave
 # Program 0 - pop the box, 1 - push the box.
-
 def run_portal(prog):
     pass
 
@@ -58,13 +56,12 @@ def run_engraver(prog):
 
 
 # change the status from 'BUSY' -> 'DONE'
-
 def empty_prog():
     pass
 
 
 def be_ready_to_switch(path, lift_id):
-    if path == 'TRUE':
+    if path:
         if lift_id == 'L3':
             pass  # go to cartesian
         elif lift_id == 'L4':
@@ -77,6 +74,8 @@ def be_ready_to_switch(path, lift_id):
 
 
 # Processes logic
+
+
 # PORTAL
 def process1(section, prog=0):
     if section[0].get_status() == 'BUSY':
@@ -90,65 +89,116 @@ def check_and_pass(section, start, end):
         pass_unit(section[start].get_stop_gate())
 
 
-def process2():
-    pass
+# DELTA
+def process2(section, prog=0):
+    if section[1].get_status() == 'BUSY':
+        read_rfid(2)
+        run_delta(prog)
 
 
-def process3():
-    pass
+# BUFFER S3 before the APAS
+def process3(section):
+    if section[2].get_status() == 'BUSY':
+        empty_prog()
 
 
-def process4():
-    pass
+# APAS
+def process4(section, prog=0):
+    if section[3].get_status() == 'BUSY':
+        read_rfid(3)
+        run_apas(prog)
 
 
-def process5():
-    pass
+# FEEDER
+def process5(section, prog=0):
+    if section[4].get_status() == 'BUSY':
+        read_rfid(4)
+        run_feeder(prog)
 
 
-def process6():
-    pass
+# EMC PRESS
+def process6(section, prog=0):
+    if section[5].get_status() == 'BUSY':
+        read_rfid(5)
+        run_emc(prog)
 
 
-def lift_enter_switch():
-    pass
+#
+def lift_enter_switch(section, path):
+    if path:
+        section[6].set_satus('WAIT')
+        be_ready_to_switch(True, 'L3')
+    else:
+        section[7].set_status('WAIT')
+        be_ready_to_switch(False, 'L3')
+    pass_unit('S6')
 
 
-def lift_exit_switch():
-    pass
+def lift_exit_switch(section, path):
+    if path:
+        section[10].set_satus('WAIT')
+        be_ready_to_switch(True, 'L4')
+        pass_unit('S7')
+    else:
+        section[11].set_status('WAIT')
+        be_ready_to_switch(False, 'L4')
+        pass_unit('S10')
 
 
-def process7():
-    pass
+# CARTESIAN
+def process7(section, prog=0):
+    if section[6].get_status() == 'BUSY':
+        read_rfid(6)
+        run_cartesian(prog)
 
 
-def process8():
-    pass
+# BUFFER S8 before the NEXO
+def process8(section):
+    if section[7].get_status() == 'BUSY':
+        empty_prog()
 
 
 def wait_nexo_button():
     pass
+    # while get_nexo_button() == False:
+    #    time.sleep(0.1) # 100ms
 
 
-def process9():
-    pass
+# NEXO
+def process9(section, prog=0):
+    if section[8].get_status() == 'BUSY':
+        read_rfid(7)
+        run_nexo(prog)
 
 
-def process10():
-    pass
+# BUFFER S10 after the NEXO
+def process10(section):
+    if section[9].get_status() == 'BUSY':
+        empty_prog()
 
 
-def process11():
-    pass
+# BUFFER S11 after the NEXO
+def process11(section):
+    if section[10].get_status() == 'BUSY':
+        empty_prog()
 
 
-def process12():
-    pass
+# ENGRAVER
+def process12(section, prog=0):
+    if section[11].get_status() == 'BUSY':
+        read_rfid(8)
+        run_engraver(prog)
 
 
-def process13():
-    pass
+# BIG BUFFER S13
+def process13(section):
+    if section[12].get_counter() > 0 and section[13].get_status() == 'IDLE':
+        section[13].set_satus('WAIT')
+        section[12].counter_decrease()
+        pass_unit('S13')
 
 
-def process14():
-    pass
+# BUFFER S14 before the PORTAL
+def process14(section):
+    if section[13].get_status() == 'BUSY':
+        empty_prog()
